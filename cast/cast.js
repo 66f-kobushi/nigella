ad80e3d4413d9dbea0976e27295c0c99a411629f
66f-kobushi <fukou.66f.1@gmail.com>
2021-07-08 20:45:41 +0900

    a

--- cast/cast.js
+++ cast/cast.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	setupMenu();
 	var userAgent = window.navigator.userAgent.toLowerCase();
-	if ((userAgent.indexOf("safari") != -1)) {
+	if ((userAgent.indexOf("safari") != -1) && (userAgent.indexOf("chrome") == -1)) {
 		$(".cast-introduction").css('background-size', 'contain');
 	}
 	
